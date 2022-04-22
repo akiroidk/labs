d9d3b3e9ab04d2bf3ee22f3e5e625a3d9ba19576
akiroidk <96384988+akiroidk@users.noreply.github.com>
2022-04-22 18:17:47 +0300

    Update lab_8.cpp

--- lab_8.cpp
+++ lab_8.cpp
@@ -1,62 +1,63 @@
-#include <iostream>
-#include <locale.h>
-#include <ctime>
+#include<iostream>
+#include<iomanip>
+#include<cstdlib>
+#include<time.h>
+
+//Дана целочисленная квадратная матрица. Опередить:
+//1) произведение элементов в тех строках, которые не содержат отрицательных элементов.
+//2) максимум среди сумм элементов диагоналей, параллельных главной диагонали матрицы.
+
 using namespace std;
-
+const int N = 4;
+const int M = 4;
 int main() {
-    setlocale(LC_ALL, "RUS");
-    srand(time(0));
+    setlocale(LC_ALL,"");
+    srand(time(NULL));
+    int Array[N][M];
 
-    int i, j, n;
-    int max_sum = 0;
-    int a[20][20];
-    cout << "Введите количество строк и столбцов: ";
-    cin >> n;
+    cout<<"Массив:";
+    for(int i=0; i<N; i++) {
+        cout<<endl;
+        for(int j=0; j<M; j++) {
+            Array[i][j] = rand()%22 - 2; //задаём рандомное значение массива
+            cout<<setw(3)<<Array[i][j];
 
-
-    cout << "Матрица: " << endl;
-
-    for (i = 0; i < n; i++) {   // заполняет массив рандомными числами
-        for (j = 0; j < n; j++)
-            a[i][j] = rand()%30-5;
-    }
-
-    for (i = 0; i < n; i++) {  // выводит матрицу
-        for (j = 0; j < n; j++) {
-            cout << a[i][j] << "\t";
         }
-        cout << endl;
     }
 
-    for (i = 0; i < n; i++) {
-        int S = 0, k = 0;
-        for (j = 0; j < n; j++) {
-            if (a[i][j] < 0) k++; //проверяет наличие отрицательных чисел
-            S += a[i][j]; // сюда записывается сумма неотрицательных элемнтов
+//2) максимум среди сумм элементов диагоналей, параллельных главной диагонали матрицы
+
+    cout<<endl;
+    int sum;
+    bool flag;
+    for (int i=0; i<N; i++) {
+        sum = 1;
+        flag = true;
+        for (int j=0; j<N; j++) {
+            if (Array[i][j]<0) {
+                flag = false;
+                break;
+            }
+            sum*=Array[i][j];
         }
-        if (k == 0) cout << "Сумма чисел в " << i + 1 << " столбце равна " << S << endl; //если она равна нулю выводим сумму
-        else cout << "В " << i + 1 << " столбце встречаются отрицательные числа" << endl; //если нет, то  это
+        if (flag)
+            cout <<"Произведение "<<i + 1 <<" строка: " <<sum<< endl;
     }
-    cout << endl;
 
+//2) максимум среди сумм элементов диагоналей, параллельных главной диагонали матрицы.
 
+    int SumMax = Array[0][N-1];
+    int SumDiag;
+    for(int i=1; i<N; i++) {
+        SumDiag=0;
+        for(int j=0; j<N-i; j++) {
 
-
-    for (int i = 0; i<n; ++i){
-        int index = i;
-        int sum = 0;
-        for (int j = 0; j<n; ++j) {
-            cout<<"a["<<i<<"]["<<j<<"]";
-            sum += a[index++][j]; // это закономерность сдвига чисел, согласно диагонали
-            if (index == n)
-                index = 0;
-            if (j != n - 1)
-                cout<<" + ";
-            if (sum>max_sum) // находим максимаоьную суму диагонали
-                max_sum=sum;
+            SumDiag+=Array[i+j][j];
         }
-        cout<<" = "<<sum<<"\n";
+        if(SumDiag>SumMax) SumMax = SumDiag;
     }
-    cout <<"Максимальная сумма элементов диагоналей = " <<max_sum<< endl;
+    cout<<"Максимальная сумма диагоналей паралельных главной = "<<SumMax<<endl;
+
+    system("pause");
     return 0;
 }
